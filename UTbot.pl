ad622f8a85646031285f3ea4a317cb982fce33e2
Andrew Huang <A.S.Huang@student.tudelft.nl>
2016-04-22 13:22:22 +0200

    Deliverable 2
    Added "Send on Change" percepts
    Bot cannot be stuck anymore
    Respawn action succesfully deployed

--- UTbot.pl
+++ UTbot.pl
@@ -3,7 +3,16 @@ navigation/2,
 navPoint/1,
 self/3,
 pickup/3,
-flag/2.
+flag/2,
+armor/4,
+game/4,
+orientation/3,
+status/4,
+score/3,
+currentWeapon/2,
+udamage/1,
+powerup/2,
+respawn/0.
 
 % We are at a certain location if the IDs match, or ...
 at(UnrealID) :- navigation(reached,UnrealID).
