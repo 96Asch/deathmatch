2c2e13c00c853a9f478488497a6d1db3e9c88225
Andrew Huang <A.S.Huang@student.tudelft.nl>
2016-04-26 00:35:15 +0200

    Deliverable 3
    When killed the agent drops all relevant goals
    The agent has specific strategies to pickup weapons,
    health and armor depending on the situation

--- UTbot.pl
+++ UTbot.pl
@@ -12,10 +12,23 @@ score/3,
 currentWeapon/2,
 udamage/1,
 powerup/2,
-respawn/0.
+respawn/0,
+recover/2,
+fragged/4,
+getArmor/2,
+item/4,
+bot/6,
+pickup/1,
+weapon/3,
+getWeapon/2,
+deploy/1,
+slowVolume/1.
 
 % We are at a certain location if the IDs match, or ...
 at(UnrealID) :- navigation(reached,UnrealID).
 % ... if the coordinates are approximately equal.
 at(location(X,Y,Z)) :- navigation(reached,location(X1,Y1,Z1)),
 	round(X) =:= round(X1), round(Y) =:= round(Y1), round(Z) =:= round(Z1).
+
+% Gives the location in the list of locations which are affected by the Slow Volume.
+pointInSlowVolume(UnrealID):- slowVolume(UnrealIDs), member(UnrealID,UnrealIDs).
